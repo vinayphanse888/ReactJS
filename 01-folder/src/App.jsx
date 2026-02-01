c167272e24a869eafef92c9362c66cd77cd45747
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-01 13:36:56 +0530

    Add 01-folder with React project

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        App
+        <h1>Hello World!</h1>
+        <br />
+        <h2>Welcome to React!</h2>
+        <br />
+        <h3>Learning React with Vite</h3>
+        <br />
+        <h4>Enjoy coding!</h4>
+        <br />
+        <h5>Happy Learning!</h5>
+        <br />
+        <h6>Have fun building your app!</h6>  
+      </div>
+    </>
+  )
+}
+
+export default App
