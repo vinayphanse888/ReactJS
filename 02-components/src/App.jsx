d3011c90d4329fcd53cd9e02b30049f8032bd17d
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-02 12:10:34 +0530

    Components in react

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <div className='card'>
+                <h2>Card Title</h2>
+                <p>This is a sample card component.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
