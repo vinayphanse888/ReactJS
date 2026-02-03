ac82a768f9fae8123b80d8e9cff2c00a932451e8
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-03 23:33:11 +0530

    commit

---
+++ 06-css/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <h3 className={styles.logo}>Hello World!</h3>
+            <button className={styles.btn}>Login</button>
+        </div>
+    )
+}
+
+export default Header
