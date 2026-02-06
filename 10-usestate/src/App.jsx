4d531c7900e5f558a36065a1ea0b3a309c4047bf
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-06 23:44:28 +0530

    useState

---
+++ 10-usestate/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [num, setNum] = useState(0)
+
+  function increaseNum() {
+    setNum(num+1)
+  }
+  function decreaseNum() {
+    setNum(num-1)
+  }
+  function jump5Num() {
+    setNum(num+5)
+  }
+  function decrease5Num() {
+    setNum(num-5)
+  }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button>
+      <button onClick={jump5Num}>Increase by 5</button>
+      <button onClick={decrease5Num}>Decrease by 5</button>
+    </div>
+  )
+}
+
+export default App
