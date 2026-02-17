11307f4301418c165d6645d734f9f1e223874ab1
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-17 19:24:45 +0530

    Advanced routing

--- 20-routing-advance/src/App.jsx
+++ 20-routing-advance/src/App.jsx
@@ -3,21 +3,32 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
-import Courses from './pages/Courses'
 import Product from './pages/Product'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
+import Men from './pages/Men'
+import Women from './pages/Women'
+import Kids from './pages/Kids'
+import Courses from './pages/Courses'
+import CourseDetail from './pages/CourseDetail'
+import Navbar2 from './components/Navbar2'
 
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='h-screen bg-black text-white'>
       <Navbar />
+      <Navbar2 />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/courses' element={<Courses />} />
-        <Route path='/product' element={<Product />} />
+        <Route path='/courses/:courseId' element={<CourseDetail />} />
 
+        <Route path='/product' element={<Product />}>
+          <Route path='men' element={<Men />} />
+          <Route path='women' element={<Women />} />
+          <Route path='kids' element={<Kids />} />
+        </Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
--- 20-routing-advance/src/pages/Product.jsx
+++ 20-routing-advance/src/pages/Product.jsx
@@ -1,9 +1,16 @@
+
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const Product = () => {
     return (
-        <div className="h-screen bg-black text-white">
-            <h1>Product page</h1>
+        <div>
+            <div className='flex justify-center gap-10 py-4'>
+                <Link className='text-xl font-semibold' to='/product/men'>Men</Link>
+                <Link className='text-xl font-semibold' to='/product/women'>Women</Link>
+                <Link className='text-xl font-semibold' to='/product/kids'>Kids</Link>
+            </div>
+            <Outlet />
         </div>
     )
 }
