9270557f2b2519d9ed71d95c86d7e9bcc7de5b99
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-18 12:28:02 +0530

    context api

---
+++ 22-context-api/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from './components/Button'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <Button />
+    </div>
+  )
+}
+
+export default App
