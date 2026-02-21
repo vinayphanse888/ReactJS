56fbc54636cc693db213a8f2b7962f5a9efb89c1
Vinay Phanse <vinayphanse14@gmail.com>
2026-02-21 20:24:55 +0530

    redux tutorial

---
+++ 23-redux-tutorial/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/features/counterSlice'
+
+const App = () => {
+
+  const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value)
+
+  const [num, setNum] = useState(5)
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => {
+        dispatch(increment())
+      }}>
+        Increment
+      </button>
+
+      <button onClick={() => {
+        dispatch(decrement())
+      }}>
+        Decrement
+      </button>
+
+      <input 
+        value={num}
+        type="number"
+        onChange={(e) => {
+          setNum(e.target.value)
+        }} 
+      />
+
+      <button onClick={() => {
+        dispatch(incrementByAmount(Number(num)))
+      }}>
+        Increase by Amount
+      </button>
+
+    </div>
+  )
+}
+
+export default App
